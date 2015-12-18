d57f3f9f4e3275150346dd4f60c61a62174e7766
halfblinded <jstroud@gmail.com>
2015-12-18 09:16:46 -0600

    Day5 commit

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,11 +1,19 @@
 #!/bin/python
 
-import re
-with open('input2', 'r') as f:
+ns = 0
+
+with open("input") as f:
 	while True:
-                c = f.readlines()
-                if not c:
-                        break
-                for line in c:
-			if 
-				print c
+		i = f.read(1)
+		if not i:
+			break
+		for line in f:
+			if line.count('a') + line.count('e') + line.count('i') + line.count('o') + line.count('u') < 3:
+				continue
+			if 'ab' in line or 'cd' in line or 'pq' in line or 'xy' in line:
+				continue
+			for c in range(0, len(line)-1):
+				if line[c] == line[c+1]:
+					ns += 1
+					break
+print ns
